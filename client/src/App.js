0fe232b3eb2c262f96c5abd9f4bff340ea9519ee
sanjayreddyarika <sanjayreddyarika@gmail.com>
2026-02-01 23:30:39 -0600

    Use HashRouter for admin route

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,19 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import CombinedPolls from "./CombinedPolls";
 import AdminPage from "./AdminPage";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div style={{ maxWidth: 520, margin: "20px auto", fontFamily: "Arial" }}>
         <h2 style={{ color: "#0f766e" }}>CHAAT Poll</h2>
 
-        {/* Admin is NOT shown publicly. You must type /admin manually */}
         <Routes>
           <Route path="/" element={<CombinedPolls />} />
           <Route path="/admin" element={<AdminPage />} />
         </Routes>
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
